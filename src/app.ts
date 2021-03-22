0c8210989c91e96944a85c8c34eab1b645ea8530
Bruno Alves <bruno.santos@ifpi.edu.br>
2021-03-21 21:12:51 -0300

    fix: add port enviroment

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
+import express from 'express';
 import dotenv from 'dotenv';
+
 dotenv.config();
 import AllocationBot from './discord/discord_connection'
 
 try {
     AllocationBot.start();
+    const server = express();
+    server.listen(process.env.PORT || 3000)
+
+    server.get('/', (req, res) => {
+        res.json({ allocation_bot: "Online" })
+    })
+
 } catch (error) {
     console.log("Não foi possivel conectar ao discord")
 }

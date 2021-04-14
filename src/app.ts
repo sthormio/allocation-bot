ad121067b2ef1328bfd852ed749ee6cf1afd4877
Bruno Alves <bruno.santos@ifpi.edu.br>
2021-04-14 11:34:19 -0300

    improvement: add ping server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 import AllocationBot from './discord/discord_connection'
+
 
 try {
     AllocationBot.start();
     const server = express();
-    server.listen(process.env.PORT || 3000)
+    server.listen(process.env.PORT || 3000, () => {
+        pingServer(20 * 60)
+    })
 
     server.get('/', (req, res) => {
         res.json({ allocation_bot: "Online ✅" })
@@ -14,4 +18,13 @@ try {
 
 } catch (error) {
     console.log("Não foi possivel conectar ao discord")
+}
+
+async function pingServer(ms: number) {
+    setTimeout(async () => {
+        await axios.get("https://allocation-bot.herokuapp.com/")
+        console.log("Ping server")
+        pingServer(ms);
+    }, ms)
+
 }
